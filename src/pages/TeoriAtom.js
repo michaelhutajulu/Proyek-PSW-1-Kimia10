1acbfdd64fdbe10ac0a386f19e7999c2a0594017
Yohananababan <yohananababan885@gmail.com>
2024-11-14 16:10:30 +0700

    Update teori atom

--- src/pages/TeoriAtom.js
+++ src/pages/TeoriAtom.js
@@ -27,6 +27,38 @@ const TeoriAtom = () => {
       <h2>Teori Atom</h2>
       <p>Penjelasan mendalam tentang teori atom akan ditampilkan di sini.</p>
     </div>
+    <div className="col-md-9">
+      <h2>Teori Atom John Dalton</h2>
+      <div className="alert alert-info">
+        <strong>Halo, Sobat pintar!</strong> Kali ini kita akan belajar tentang <strong>Perkembangan Teori Atom</strong>. Ada <strong>Teori Atom Dalton</strong> sebagai pembuka nih, Sobat!
+      </div>
+      <p>
+        Struktur atom adalah susunan partikel dasar dalam atom. Atom sebagai partikel penyusun materi dapat menentukan sifat materi. 
+        <strong> Atom </strong> berasal dari Bahasa Yunani yaitu <i>atomos</i> yang berarti partikel terkecil yang tidak dapat dibagi lagi.
+        Atom memang tidak dapat dilihat dengan kasat mata, namun kita dapat melihat visualisasi atom dalam
+        <strong> perkembangan model atom </strong> berdasarkan hasil eksperimen dari para ilmuwan. Kebenaran suatu model atau teori atom
+        tidak mutlak. Dalam perkembangannya model atau teori atom terus mengalami perbaikan dan penyempurnaan.
+      </p>
+
+      <h3>Teori atom John Dalton</h3>
+      <img src="John Dalton.jpg" alt="Model Atom Dalton" className="img-fluid mb-3" />
+
+      <p>Dalton mengemukakan tentang atom sebagai berikut:</p>
+      <ul>
+        <li>Atom adalah bola pejal yang tidak bermuatan.</li>
+        <li>Atom sebagai terkecil suatu materi yang tidak dapat dibagi lagi.</li>
+        <li>Atom dari suatu unsur yang sama memiliki berat, ukuran dan bentuk yang sama, tetapi berbeda dengan atom-atom lain.</li>
+        <li>Atom tidak dapat diciptakan dan dimusnahkan.</li>
+        <li>Pada reaksi kimia terjadi penggabungan dan pemisahan atom.</li>
+        <li>Senyawa adalah hasil reaksi atom-atom penyusunnya.</li>
+      </ul>
+
+      <h4>Kelebihan</h4>
+      <ul>
+        <li>Meningkatkan rasa minat terhadap penelitian model atom berikutnya.</li>
+        <li>Mampu menjelaskan Hukum Kekekalan Massa, Hukum Perbandingan Tetap, dan Hukum Perbandingan Berganda.</li>
+      </ul>
+      </div>
     </div>
   );
 };
