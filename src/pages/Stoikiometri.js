b3c24a4be2587b4e24efbea0465b999e840eec26
Gery-gif-15 <nbgery@gmail.com>
2024-11-19 11:22:17 +0700

    update materi stoikimetri beserta gambarnya

--- src/pages/Stoikiometri.js
+++ src/pages/Stoikiometri.js
@@ -25,8 +25,35 @@ const Stoikiometri = () => {
       </nav>
     <div className="page-container">
       <h2>Stoikiometri</h2>
+      <div className="image-container">
+      <img src="/Stoikiometri.jpg" 
+        alt="Stoikiometri" 
+        style={{  width: '50%', height: 'auto', marginTop: '20px' }}
+        />
+      </div>
       <p>Penjelasan dan contoh soal stoikiometri akan ditampilkan di sini.</p>
+      <h3>Pengertian Stoikiometri</h3>
+      <p> Stoikometri, di dalam ilmu kimia ialah ilmu yang mempelajari hubungan kuantitatif antar
+          reaktan dan produk dalam reaksi kimia. Konsep stoikometri digunakan untuk menghitung massa, volume, dan 
+          jumlah mol zat yang terlibat dalam suatu reaksi kimia. dan stoikometri berasal dari kata "stoicheion" dalam 
+          bahasa Yunani yang berarti mengukur.</p>
+      <p>
+      Suatu reaksi kimia dapat dikatakan sebagai reaksi
+      stoikometri apabila reaktan dalam reaksi habis seluruhnya.
+      </p>
+      <p>Adapun rumus-rumus yang biasa digunakan dalam menyelesaikan 
+      materi Kimia Stoikiometri adalah sebagai berikut:
+      </p>
     </div>
+    <img 
+    src="/Rumus-Stoikiometri-Kimia.png"  
+    alt="Gambar"
+    style={{  width: '50%', height: 'auto', marginTop: '20px' }}
+    />
+    <p>Angka 22,4 L merupakan volume gas ideal dalam keadaan STP (Standard Temperature and Pressure), 
+      dengan tekanan gas (P) = 1 atm, dan suhu (T) = 273 K.
+       Sementara angka 6,02 x 1023 merupakan besaran tetapan Avogadro. 
+       Jadi, 1 mol zat apa pun memiliki jumlah partikel yang sama yaitu sebanyak 6,02 x 1023 partikel.</p>
     </div>
   );
 };
