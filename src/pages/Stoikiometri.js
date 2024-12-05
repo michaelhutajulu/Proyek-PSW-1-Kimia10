fd92fcfc844b621a6bb11282e1510b519ee1c6e9
Gery-gif-15 <nbgery@gmail.com>
2024-12-05 15:00:21 +0700

    Update Stoikiometri.js

--- src/pages/Stoikiometri.js
+++ src/pages/Stoikiometri.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import './Stoikiometri.css';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
 
 const Stoikiometri = () => {
   const scrollToSection = (id) => {
